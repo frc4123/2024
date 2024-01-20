6e8d41527f0511ffe8f8af72a9d205c98dc81d3e
AlphaLQ <sjb2019dpo@gmail.com>
2024-01-20 13:34:52 -0800

    correct inversions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,9 +53,9 @@ public final class Constants {
     public static final boolean Back_Right_Drive_Absolute_Encoder_Reversed = false;
     // drive encoders not reversed
 
-    public static final double kTrackWidth = Units.inchesToMeters(21);
+    public static final double kTrackWidth = Units.inchesToMeters(20.75);
     // Distance between right and left wheels
-    public static final double kWheelBase = Units.inchesToMeters(25.5);
+    public static final double kWheelBase = Units.inchesToMeters(20.75);
     // Distance between front and back wheels
 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
@@ -102,13 +102,13 @@ public final class Constants {
   public static final class ModuleConstants {
     
     public static final double kWheelDiameterMeters = Units.inchesToMeters(4); //wheel diameter
-    public static final double kDriveMotorGearRatio = 6.75 / 1; // motor gear ratio
-    public static final double TurningMotorGearRatio = (150/7) / 1 ; // turning gear ratio
+    public static final double kDriveMotorGearRatio =  1 / 6.75; // motor gear ratio
+    public static final double TurningMotorGearRatio = 1.0 / (150/7); // turning gear ratio
     public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
     public static final double kTurningEncoderRot2Rad = TurningMotorGearRatio * 2 * Math.PI;
     public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
     public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
-    public static final double kPTurning = 0.5;
+    public static final double kPTurning = 0.3; //
     public static final double kPhysicalMaxSpeedMetersPerSecond = 5;
     // the above will have to be changed after gear ratio measurements, wheel measurements, and pid tuning
     
