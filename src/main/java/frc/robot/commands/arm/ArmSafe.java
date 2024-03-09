8f9b454802636959a137e752fbf91a3bf5d7a6ed
AlphaLQ <sjb2019dpo@gmail.com>
2024-03-08 18:02:56 -0800

    new arm safety setpoint

---
+++ src/main/java/frc/robot/commands/arm/ArmSafe.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.PIDTuning;
+import frc.robot.subsystems.Arm;
+
+public class ArmSafe extends Command{
+    Arm arm;
+
+    public ArmSafe(Arm arm){
+        this.arm = arm;
+        addRequirements(arm);
+    }
+
+    @Override
+    public void execute() {
+        arm.setPosition(PIDTuning.Arm_POSITION_SAFE);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+}
