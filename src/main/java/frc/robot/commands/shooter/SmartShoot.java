3ef8ff363060254c6116b8a8eacfc5c74e61a8c7
AlphaLQ <sjb2019dpo@gmail.com>
2024-03-27 19:09:52 -0700

    hide

--- src/main/java/frc/robot/commands/shooter/SmartShoot.java
+++ src/main/java/frc/robot/commands/shooter/SmartShoot.java
@@ -1,42 +1,42 @@
-package frc.robot.commands.shooter;
+// package frc.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Vision;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.InstantCommand;
+// import edu.wpi.first.wpilibj2.command.WaitCommand;
+// import frc.robot.subsystems.Shooter;
+// import frc.robot.subsystems.Vision;
 
-public class SmartShoot extends Command{
+// public class SmartShoot extends Command{
 
-    Shooter shooter;
-    Vision vision;
+//     Shooter shooter;
+//     Vision vision;
 
-    public SmartShoot(Shooter shooter, Vision vision) {
-        this.shooter = shooter;
-        this.vision = vision;
+//     public SmartShoot(Shooter shooter, Vision vision) {
+//         this.shooter = shooter;
+//         this.vision = vision;
 
-        addRequirements(vision);
-        addRequirements(shooter);
-    }
+//         addRequirements(vision);
+//         addRequirements(shooter);
+//     }
 
-    @Override
-    public void execute() {
-        shooter.setShooterVelo(0.9); 
-    }
+//     @Override
+//     public void execute() {
+//         shooter.setShooterVelo(-0.7); 
+//     }
     
-    @Override
-    public void end(boolean interrupted) {
-        shooter.setShooterVelo(0.0); 
-    }
+//     @Override
+//     public void end(boolean interrupted) {
+//         shooter.setShooterVelo(0.0); 
+//     }
 
 
 
-    public void periodic(){
-        if (vision.isNearShooter()) {
-            execute();
-        } else if (vision.hasTarget()){
-            new WaitCommand(1)
-            .andThen( new InstantCommand(() -> end(true)));
-        }
-    }
-}
+//     public void periodic(){
+//         if (vision.isNearShooter()) {
+//             execute();
+//         } else if (vision.hasTarget()){
+//             new WaitCommand(1)
+//             .andThen( new InstantCommand(() -> end(true)));
+//         }
+//     }
+// }
