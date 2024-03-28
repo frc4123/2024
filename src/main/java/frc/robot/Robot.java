7cbd4a97d81d605d168d4f021cdbdff7a165c60b
AlphaLQ <sjb2019dpo@gmail.com>
2024-03-27 19:08:32 -0700

    removed unused imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.shooter.AutoShooter;
+import frc.robot.subsystems.Shooter;
 
 public class Robot extends TimedRobot {
 
